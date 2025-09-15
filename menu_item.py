ca1d1a1eecdbf1cee1655c8032d7cf6dd148b4aa
loliplayz22 <okoko26@nmhschool.org>
2025-09-14 23:46:46 -0400

    Update menu_item.py

--- menu_item.py
+++ menu_item.py
@@ -1,2 +1,12 @@
-import json 
-print("Loli")
+class MenuItem:
+
+    def __init__(self, item_id, name, price):
+        self.id = item_id  # Changed from 'item_id' to 'id'
+        self.name = name
+        self.price = price
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "price": self.price}
+    
+    def __str__(self):
+        return f"{self.id}: {self.name} - ${self.price:.2f}"
